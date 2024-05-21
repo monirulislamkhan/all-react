c271ef8e70df3a6ed39582946b6289b1068b0ff5
monirulislamkhan <mr.moninrulkhan@gmail.com>
2024-05-21 19:48:31 +0530

    step work stated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Steps />
+    </>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  function prevClickHandler() {
+    if (step > 1) setStep((set) => set - 1);
+  }
+
+  function nextClickHandler() {
+    if (step < 3) setStep((set) => set + 1);
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`step ${step >= 1 ? "active" : ""}`}>{1}</div>
+        <div className={`step ${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`step ${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <div className="message">
+        <h3>
+          Step {step}: {messages[step - 1]}
+        </h3>
+      </div>
+
+      <div className="buttons">
+        <button onClick={prevClickHandler}>Previous</button>
+        <button onClick={nextClickHandler}>Next</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+// function Message() {
+//   return (
+//     <div className="message">
+//       <h3>Step</h3>
+//     </div>
+//   );
+// }
+
+function Button({ children }) {
+  return <button>{children}</button>;
+}
